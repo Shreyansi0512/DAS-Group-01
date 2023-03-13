eada57b258f7cd129c5d8ec3e4f58a8c6bce7960
HermioneHQJ <127128019+HermioneHQJ@users.noreply.github.com>
2023-03-13 17:43:35 +0000

    Update Logistics.R

--- Logistics.R
+++ Logistics.R
@@ -30,4 +30,20 @@ pre_logistic<-predict(dt_logistic,newdata=test_data,type="response")
 print(pre_logistic)
 obs_p_logistic = data.frame(prob=pre_logistic,obs=test_data$Total.Number.of.Family.members)
 table(test_data$Total.Number.of.Family.members,pre_logistic,dnn=c("real","pred"))
-logistic_roc <- roc(test_data$Total.Number.of.Family.members,pre_logistic)
+
+# Create a binary response variable by collapsing some levels
+test_data$binary_response <- ifelse(test_data$Total.Number.of.Family.members > 2, "Positive", "Negative")
+
+# Generate predicted probabilities using the logistic regression model
+pre_logistic <- predict(dt_logistic, newdata = test_data, type = "response")
+
+# Calculate the ROC curve using the binary response variable and predicted probabilities
+logistic_roc <- roc(test_data$binary_response, pre_logistic)
+
+# Plot the ROC curve
+plot(logistic_roc)
+
+# we collapse the original response variable test_data$Total.Number.of.Family.members into a binary variable with two levels
+# where "Positive" indicates the response variable is greater than 2, and "Negative" indicates the response variable is less than or equal to 2
+# We then generate predicted probabilities using the logistic regression model dt_logistic, and use the roc function to calculate the ROC curve 
+# using the new binary response variable and predicted probabilities. Finally, we plot the resulting ROC curve using the plot function
